d3f6d70e27dccef50cb0e1f9bec31cfc48b20afe
vexra <112993677+vexra@users.noreply.github.com>
2025-05-22 04:21:07 -0400

    first commit

---
+++ app/Models/Batch.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Batch extends Model
+{
+    use HasFactory;
+    protected $table = 'batches';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'course_id', 'start_date'];
+
+    public function course(){
+        return $this->belongsTo(Course::class);
+    }
+}
---
+++ app/Models/Enrollment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    use HasFactory;
+    protected $table = 'enrollments';
+    protected $primaryKey = 'id';
+    protected $fillable = ['enroll_no', 'batch_id', 'student_id', 'join_date', 'fee'];
+
+    public function student(){
+        return $this->belongsTo(Student::class);
+    }
+    public function batch(){
+        return $this->belongsTo(Batch::class);
+    }
+}
