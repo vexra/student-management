e7a2fcf43fbbb407ff4e882cbea6dcda584b2628
vexra <112993677+vexra@users.noreply.github.com>
2025-05-25 03:19:43 -0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Layout');
+    return view('layout');
 });
 Route::resource('/students', StudentController::class);
 
