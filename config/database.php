76a2645df03da5977513b44d08249eea355b8994
vexra <112993677+vexra@users.noreply.github.com>
2025-05-25 11:24:51 -0400

    fix

--- config/database.php
+++ config/database.php
@@ -76,9 +76,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
-            'options' => extension_loaded('pdo_pgsql') ? array_filter([
-                PDO::PGSQL_ATTR_SSL_MODE => 'require',
-            ]) : [],
         ],
 
         'sqlsrv' => [
